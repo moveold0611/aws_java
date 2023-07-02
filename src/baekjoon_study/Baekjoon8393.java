beceb7599fbdfec2c426e01e3b50b743939c7c29
moveold0611 <b44sup97045@gmail.com>
2023-07-02 20:29:56 +0900

    feat: 2023_07_02 백준

---
+++ src/baekjoon_study/Baekjoon8393.java
@@ -0,0 +1,28 @@
+package baekjoon_study;
+
+import java.util.Scanner;
+
+public class Baekjoon8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d;
+		a = sc.nextInt();		
+		c = 0;
+				
+		for(int i = 0; i < 10000; i++) {				
+			b = i + 1;			
+			c = b + c;
+			d = b + c;
+			if(i + 1 == a) {
+				System.out.println(c);
+				break;
+			}						
+		}
+		
+		
+
+	}
+
+}
