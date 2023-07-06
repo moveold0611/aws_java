cf191de53bfef54699a72d31df4e7fa5a549e832
moveold0611 <b44sup97045@gmail.com>
2023-07-06 16:08:59 +0900

    feat: 03_07_06 백준

---
+++ src/baekjoon_study/Baekjoon2562.java
@@ -0,0 +1,32 @@
+package baekjoon_study;
+
+import java.util.Scanner;
+
+public class Baekjoon2562 {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		int max = 0;
+		int count = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			int put = sc.nextInt();			
+			if(max < put) {
+				max = put;
+				count = i + 1;
+			}else if(9 == i + 1) {								
+				System.out.println("가장 큰 수는 " + count + "번째 수 " + max + "입니다.");
+			}
+		}
+		
+				
+		
+
+		
+		
+		
+
+	}
+
+}
