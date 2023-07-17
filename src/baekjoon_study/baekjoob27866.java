83449a33511df9690cca649d61d2bf7994c9244e
moveold0611 <b44sup97045@gmail.com>
2023-07-17 17:37:23 +0900

    no message

--- src/baekjoon_study/baekjoob27866.java
+++ src/baekjoon_study/baekjoob27866.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class baekjoob27866 {
 
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		
-//		String str = null;
-//		
-//		for (int i = 0; i < 3;  i++) {
-//			str = sc.next();
-//			int a = sc.nextInt();
-//			str.substring(a);
-//			System.out.println(str.substring(a - 1, a));
-//		}
-//		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = null;
+		
+		for (int i = 0; i < 3;  i++) {
+			str = sc.next();
+			int a = sc.nextInt();
+			str.substring(a);
+			System.out.println(str.substring(a - 1, a));
+		}
+		
 
 	}
 
